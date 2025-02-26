96077bef71c339b1ae3de33b9b209e83330acaf2
Lucas Lelieur <lucas.lelieur.ll@gmail.com>
2025-02-26 21:16:46 +0100

    Fixing errors

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,23 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from "dotenv";
 
-import './db';
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: ".env.local" });
+} else {
+  dotenv.config({ path: ".env" });
+}
 
-import express, { Application } from 'express';
+import "./db";
+
+import express, { Application } from "express";
 const app: Application = express();
 
-import configure from './config';
+import configure from "./config";
 configure(app);
 
-import routes from './routes';
+import routes from "./routes";
 routes(app);
 
-import errorHandling from './error-handling';
+import errorHandling from "./error-handling";
 errorHandling(app);
 
 export default app;
