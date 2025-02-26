1d773fc239c6cb507e0bf3a551ff42ee969b44bd
Lucas Lelieur <lucas.lelieur.ll@gmail.com>
2025-02-26 19:34:53 +0100

    Fixing errors

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,10 @@ import logger from "morgan";
 import cors from "cors";
 
 const FRONTEND_URL =
-  process.env.ORIGIN || "https://tailor-challenge-frontend.vercel.app/signup";
+  process.env.NODE_ENV === "production"
+    ? process.env.ORIGIN ||
+      "https://tailor-challenge-frontend.vercel.app/signup"
+    : "http://localhost:5173"; // URL para el entorno local
 
 export default (app: Application): void => {
   app.set("trust proxy", 1);
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,14 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const MONGODB_URI: string = process.env.MONGODB_URI as string || 'mongodb://localhost:27017/';
+const MONGODB_URI: string =
+  (process.env.MONGODB_URI as string) || "mongodb://localhost:27017/";
 
 mongoose
-    .connect(MONGODB_URI)
-    .then((x) => {
-        const dbName: string = x.connections[0].name;
-        console.log(`Connected to Mongo! Database name: "${dbName}"`);
-    })
-    .catch((err: Error) => {
-        console.error("Error connecting to mongo: ", err);
-    });
+  .connect(MONGODB_URI)
+  .then((x) => {
+    const dbName: string = x.connections[0].name;
+    console.log(`Connected to Mongo! Database name: "${dbName}"`);
+  })
+  .catch((err: Error) => {
+    console.error("Error connecting to mongo: ", err);
+  });
