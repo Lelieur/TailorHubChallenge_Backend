028cf4c5b7723a10a081cd3cee9295b9782d4582
Lucas Lelieur <lucas.lelieur.ll@gmail.com>
2025-02-27 14:56:13 +0100

    Changed MONGO URI to a connected MONGO DB database

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const MONGODB_URI: string =
-  (process.env.MONGODB_URI_PROD as string) ||
-  "mongodb://localhost:27017/TailorChallenge";
+  (process.env.MONGODB_URI as string) ||
+  "mongodb+srv://lucaslelieurll:Lukyluk1.72117@tailorhubchallenge.d9tp1.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
   .connect(MONGODB_URI)
